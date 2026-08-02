083f920cc1f74678578a60da1ef13dd590969ab0
buildbylogic-netizen <buildbylogic@gmail.com>
2026-08-02 12:11:48 -0700

    Create  NumberGuessingGame.java

---
+++ NumberGuessingGame.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.numberguessinggame;
+
+import com.mycompany.numberguessinggame.ui.WelcomeFrame;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+
+        WelcomeFrame frame = new WelcomeFrame();
+        frame.setVisible(true);
+
+    }
+}
